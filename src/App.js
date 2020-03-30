92328ee56c0e6f333a3b1087ce528316e966f82d
Ted Ahlberg <ted.ahlberg95@gmail.com>
2020-03-30 14:14:39 +0200

    clean-up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      asd
+    </div>
+  );
+}
+
+export default App;
---
+++ src/intro/Intro.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum
+      </h1>
+      <span>
+        dolor sit amet, consectetur adipiscing elit. Nullam odio leo, molestie nec justo nec, elementum varius justo. In id justo in orci blandit interdum. Curabitur ornare ut risus in rutrum. Aenean tristique lacus laoreet turpis facilisis congue id vel lectus. Integer ut odio fermentum, dignissim ante eget, placerat dolor. Pellentesque placerat mattis dui. Sed et enim eu massa tincidunt rhoncus. Etiam blandit laoreet tincidunt. Proin pellentesque dolor sit amet sapien euismod, vel mollis sem suscipit. In hac habitasse platea dictumst. In scelerisque felis in dignissim posuere. Cras ullamcorper justo ac tristique pharetra. Quisque sagittis ex sit amet eros tempor euismod a luctus ligula.
+      </span>
+      <button>
+        Starta
+      </button>
+    </div>
+  )
+}
+
+export default Intro
