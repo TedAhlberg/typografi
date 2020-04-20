67f4fb2c5713ec584bb992eb896c43d4eedcc2e8
Ted Ahlberg <ted.ahlberg95@gmail.com>
2020-04-20 16:05:57 +0200

    IntroSurvey styling

--- src/components/pages/Intro.js
+++ src/components/pages/Intro.js
@@ -7,7 +7,7 @@ class Intro extends React.Component {
         <h1>Examensarbete</h1>
         <h2>Ted Ahlberg</h2>
         <p>
-        Som en del i mitt examensarbete ska jag undersöka användbarheten och utformandet av texter för mobila enheter. För att kunna undersöka detta har jag utformat denna webbapplikation, applikationen möjliggör insamlingen av data för undersökningen. All data som samlas in om deltagaren är helt anonym och kommer inte kunna spåras tillbaka till deltagaren. 
+        Som en del i mitt examensarbete ska jag undersöka användbarheten och utformandet av texter för mobila enheter. För att kunna undersöka detta har jag utvecklat denna webbapplikation, applikationen möjliggör insamlingen av data för undersökningen. All data som samlas in om deltagaren är helt anonym och kommer inte kunna spåras tillbaka till deltagaren. 
         </p>
         <p>
         Deltagarna av underökningen blir ombedda att läsa 7 artiklar och svara på en kort enkät efter varje artikel, hela genomförandet tar cirka XX minuter. Om du som deltagare behöver eller vill ta en paus under genomförandet så går det bra, se bara till att göra det under en enkät och inte under läsandet av en artikel.
--- src/components/pages/IntroSurvey.js
+++ src/components/pages/IntroSurvey.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './IntroSurvey.css'
 
 class IntroSurvey extends React.Component {
   constructor(props) {
@@ -26,37 +27,85 @@ class IntroSurvey extends React.Component {
 
   render() {
     return (
-      <div>
-        <span>input numbers</span>
+      <div id="introSurveyContainer">
+        <h1>Inledande enkät</h1>
+
+        <span>Hur gammal är du?</span>
         <input type="text"
           value={this.state.number}
           onChange={this.handleNumberChange} />
 
-        <span>rg1</span>
-        <input type="radio" name="rg1" value="1"
-          checked={this.state.rg1 === "1"}
-          onChange={this.handleOptionChange} />
+        <span>Hur mycket läser du under en vecka?</span>
+        <div className="radioGroup">
+          <div className="radioChoiceContainer">
+            <input type="radio" name="rg1" value="1"
+              className="radioChoice"
+              checked={this.state.rg1 === "1"}
+              onChange={this.handleOptionChange} />
+            <span>Mindre än 1 timme</span>
+          </div>
 
-        <input type="radio" name="rg1" value="2"
-          checked={this.state.rg1 === "2"}
-          onChange={this.handleOptionChange} />
+          <div className="radioChoiceContainer"
 
-        <input type="radio" name="rg1" value="3"
-          checked={this.state.rg1 === "3"}
-          onChange={this.handleOptionChange} />
+            onClick={this.handleOptionChange}>
+            <input type="radio" name="rg1" value="2"
+              className="radioChoice"
+              checked={this.state.rg1 === "2"}
+              onChange={this.handleOptionChange} />
+            <span>1 - 3 timmar</span>
+          </div>
 
-        <span>rg2</span>
-        <input type="radio" name="rg2" value="1"
-          checked={this.state.rg2 === "1"}
-          onChange={this.handleOptionChange} />
+          <div className="radioChoiceContainer">
+            <input type="radio" name="rg1" value="3"
+              className="radioChoice"
+              checked={this.state.rg1 === "3"}
+              onChange={this.handleOptionChange} />
+            <span>3 - 6 timmar</span>
+          </div>
 
-        <input type="radio" name="rg2" value="2"
-          checked={this.state.rg2 === "2"}
-          onChange={this.handleOptionChange} />
+          <div className="radioChoiceContainer">
+            <input type="radio" name="rg1" value="4"
+              className="radioChoice"
+              checked={this.state.rg1 === "4"}
+              onChange={this.handleOptionChange} />
+            <span>6 - 10 timmar</span>
+          </div>
 
-        <input type="radio" name="rg2" value="3"
-          checked={this.state.rg2 === "3"}
-          onChange={this.handleOptionChange} />
+          <div className="radioChoiceContainer">
+            <input type="radio" name="rg1" value="5"
+              className="radioChoice"
+              checked={this.state.rg1 === "5"}
+              onChange={this.handleOptionChange} />
+            <span>Mer än 10 timmar</span>
+          </div>
+        </div>
+
+        <span>Vilket format läser du mest av?</span>
+        <div className="radioGroup">
+          <div className="radioChoiceContainer">
+            <input type="radio" name="rg2" value="1"
+              className="radioChoice"
+              checked={this.state.rg2 === "1"}
+              onChange={this.handleOptionChange} />
+            <span>Digitalt format</span>
+          </div>
+
+          <div className="radioChoiceContainer">
+            <input type="radio" name="rg2" value="2"
+              className="radioChoice"
+              checked={this.state.rg2 === "2"}
+              onChange={this.handleOptionChange} />
+            <span>Tryckt format</span>
+          </div>
+
+          <div className="radioChoiceContainer">
+            <input type="radio" name="rg2" value="3"
+              className="radioChoice"
+              checked={this.state.rg2 === "3"}
+              onChange={this.handleOptionChange} />
+            <span>Ungefär lika mycket</span>
+          </div>
+        </div>
 
         <button onClick={this.onNext}>
           Starta
