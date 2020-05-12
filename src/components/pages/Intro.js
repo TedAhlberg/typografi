9413e7df86a66b9e344fa74d0a6f9050e4d7d7ab
Ted Ahlberg <ted.ahlberg95@gmail.com>
2020-05-12 09:56:46 +0200

    quick intro text fix

--- src/components/pages/Intro.js
+++ src/components/pages/Intro.js
@@ -7,10 +7,10 @@ class Intro extends React.Component {
         <h1>Examensarbete</h1>
         <h2>Ted Ahlberg</h2>
         <p>
-        Som en del i mitt examensarbete ska jag undersöka användbarheten och utformandet av texter för mobila enheter. För att kunna undersöka detta har jag utvecklat denna webbapplikation, applikationen möjliggör insamlingen av data för undersökningen. All data som samlas in under deltagandet är helt anonym och kommer inte kunna spåras tillbaka till deltagaren.
+          Som en del i mitt examensarbete ska jag undersöka användbarheten och utformandet av texter för mobila enheter. För att kunna undersöka detta har jag utvecklat denna webbapplikation, applikationen möjliggör insamlingen av data för undersökningen. All data som samlas in under deltagandet är helt anonym och kommer inte kunna spåras tillbaka till deltagaren.
         </p>
         <p>
-          Deltagarna av underökningen blir ombedda att läsa 7 artiklar och svara på en kort enkät efter varje artikel, hela genomförandet tar cirka 15 minuter. Undersökningen är uppdelad i två delar, de 4 första artiklarna avhandlar typsnitt medan de 3 sista artiklarna avhandlar typstorlek.
+          Deltagarna av underökningen blir ombedda att läsa 7 artiklar och svara på en kort enkät efter varje artikel, hela genomförandet tar cirka 15 minuter. Undersökningen är uppdelad i två delar, de 4 första artiklarna avhandlar typsnitt medan de 3 sista artiklarna avhandlar typstorlek. När en artikel är färdigläst och deltagaren gått vidare går det inte att gå tillbaka till artikeln igen, tillbaka-knappen i webbläsaren återställer genomförandet.
         </p>
         <p>
           Applikationen ska genomföras på en mobiltelefon och inte på en PC eller laptop. Om du som deltagare behöver eller vill ta en paus under genomförandet så går det bra, se bara till att göra det under en enkät och inte under läsandet av en artikel.
