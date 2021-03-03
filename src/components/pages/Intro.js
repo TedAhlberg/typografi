56cdd34cc9dfd3bffde33bf4cf32fa8672e5f0bb
Ted Ahlberg <ted.ahlberg95@gmail.com>
2021-03-03 09:31:14 +0100

    Intro fix

--- src/components/pages/Intro.js
+++ src/components/pages/Intro.js
@@ -7,13 +7,13 @@ class Intro extends React.Component {
         <h1>Examensarbete</h1>
         <h2>Ted Ahlberg</h2>
         <p>
-          Som en del i mitt examensarbete ska jag undersöka användbarheten och utformandet av texter för mobila enheter. För att kunna undersöka detta har jag utvecklat denna webbapplikation, applikationen möjliggör insamlingen av data för undersökningen. All data som samlas in under deltagandet är helt anonym och kommer inte kunna spåras tillbaka till deltagaren.
+          Som en del i mitt examensarbete ska jag undersöka läsbarheten och utformandet av texter för mobila enheter. För att kunna undersöka detta har jag utvecklat denna webbapplikation, vilket möjliggör insamlingen av data som kommer ligga till underlag för arbetet. All data som samlas in är helt anonym och kan inte spåras tillbaka till deltagaren.
         </p>
         <p>
-          Deltagarna av underökningen blir ombedda att läsa 7 artiklar och svara på en kort enkät efter varje artikel, hela genomförandet tar cirka 15 minuter. Undersökningen är uppdelad i två delar, de 4 första artiklarna avhandlar typsnitt medan de 3 sista artiklarna avhandlar typstorlek. När en artikel är färdigläst och deltagaren gått vidare går det inte att gå tillbaka till artikeln igen, tillbaka-knappen i webbläsaren återställer genomförandet.
+          Deltagaren av undersökningen blir ombedd att läsa 7 artiklar och svara på en kort enkät efter varje artikel, hela genomförandet tar cirka 15 minuter. Undersökningen är uppdelad i två delar, varav de 4 första artiklarna avhandlar typsnitt medan de 3 sista artiklarna avhandlar typstorlek. När en artikel är färdigläst och deltagaren gått vidare går det inte att gå tillbaka till föregående artikel, tillbaka-knappen i webbläsaren återställer hela genomförandet.
         </p>
         <p>
-          Applikationen ska genomföras på en mobiltelefon och inte på en PC eller laptop. Om du som deltagare behöver eller vill ta en paus under genomförandet så går det bra, se bara till att göra det under en enkät och inte under läsandet av en artikel.
+        Applikationen ska genomföras på en mobiltelefon och inte på en PC eller laptop. Om du som deltagare behöver ta en mindre paus under genomförandet så går det bra, se bara till att göra det under en enkät och inte under läsandet av en artikel. Använder du vanligtvis läsglasögon ombeds du att även använda dessa under deltagandet i undersökningen.
         </p>
         <button onClick={this.props.onNext}>
           Nästa
